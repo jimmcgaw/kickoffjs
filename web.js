5a20ef8cdebe27c8be55257d4dd04fc915c05bbe
Jim McGaw <smoochy@Jim-McGaws-MacBook-Pro.local>
2013-07-16 23:12:26 -0700

    organize code to be slightly less hackish

--- web.js
+++ web.js
@@ -3,15 +3,17 @@ var express = require("express");
 var htmlparser = require("htmlparser");
 var select = require('soupselect').select;
 var _ = require('underscore');
-var mustache = require("mustache");
 
 var app = express();
 app.use(express.logger());
 
+app.use("/scripts", express.static(__dirname + '/scripts'));
+app.use("/images", express.static(__dirname + '/images'));
+
 var KICKOFF_URL = "http://www.meetup.com/Santa-Barbara-JavaScript-Meetup/events/125332992/";
 
 app.get('/', function(request, response) {
-  response.end("<!DOCTYPE html><html><head></head><body><h1>JavaScript Kickoff Member List</h1><br /><a href='/members'>Load member list</a></body></html>");
+  response.sendfile("index.html");
 });
 
 app.get('/members', function(request, response) {
@@ -21,22 +23,20 @@ app.get('/members', function(request, response) {
       if (error) {
         console.log(error);
       } else {
-        var members = select(dom, "#rsvp-list h5.member-name a"),
-            i;
-        console.log("Found " + members.length + " members.");
+        var members = select(dom, "#rsvp-list h5.member-name a");
         member_list = _.map(members, function(member){
           return { "name" : member.children[0].raw };
         });
-        var view = { "members" : member_list };
-        response.end(mustache.render("<!DOCTYPE html><html><head></head><body><h2>{{members.length}} member(s) are attending:</h2><br />{{#members}}{{ name }}<br />{{/members}}</body></html>", view));
+
+        response.writeHead(200, { 'Content-Type': 'application/json' });
+        response.write(JSON.stringify(member_list));
+        response.end();
       }
     });
     var parser = new htmlparser.Parser(handler);
     parser.parseComplete(body);
   });
 });
-
-
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
