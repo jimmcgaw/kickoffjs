2ed73c9148794c0464672493b534ada6a7e7cf29
Jim McGaw <smoochy@Jim-McGaws-MacBook-Pro.local>
2013-07-17 20:50:10 -0700

    random drawing

--- scripts/index.js
+++ scripts/index.js
@@ -12,6 +12,20 @@ $(function(){
 				ul.append(li);
 			});
 			$("#content").html(ul);
+			$("#pick").show();
+		}
+	});
+
+	$("#pick").click(function(){
+		var lis = $("#content").find("li");
+		var count = lis.length;
+		var random_member_index = Math.floor(Math.random()*(count+1));
+		var winner = $(lis[random_member_index]);
+		if (winner.text() === "Joshua Ball"){
+			alert("Josh Ball cannot win this!");
+		} else {
+			winner.css("font-weight", "bold");
+		winner.append(" <-- winner winner chicken dinner!");
 		}
 	});
 });
